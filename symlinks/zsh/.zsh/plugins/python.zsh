37e741691671b9cc6795fcd3e7ce7f537b18f9d2
yowatari <4982161+YOwatari@users.noreply.github.com>
2021-11-24 02:48:45 +0900

    fix pipx completions (#7)

--- symlinks/zsh/.zsh/plugins/python.zsh
+++ symlinks/zsh/.zsh/plugins/python.zsh
@@ -5,7 +5,10 @@ path=($HOME/.local/bin(N-/) $path)
 _zsh_pipx_install() {
   echo "Installing pipx..."
   python3 -m pip install --user pipx
-  pipx completions > "$HOME/.zsh/completions/_pipx"
+}
+
+_zsh_pipx_load() {
+  eval "$(register-python-argcomplete pipx)"
 }
 
 _zsh_poetry_install() {
@@ -17,6 +20,10 @@ _zsh_poetry_install() {
 
 if ! command -v pipx &>/dev/null; then
   _zsh_pipx_install
+fi
+
+if command -v pipx &>/dev/null; then
+  _zsh_pipx_load
 fi
 
 if ! command -v poetry &>/dev/null; then
