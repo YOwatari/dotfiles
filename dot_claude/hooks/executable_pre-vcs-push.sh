76381f615bdc0ba886d2e76155146d2b5610a622
Yuta OHWATARI <y.ohwatari@cartahd.com>
2026-02-22 16:52:07 +0900

    chore: remove obsolete hook scripts for pre-compact, VCS push, session end, start, and suggest-compact to clean up deprecated automation and prevent unintended executions

--- dot_claude/hooks/executable_pre-vcs-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Pre-VCS-push hook: Pause for review before pushing (git/jj)
-
-echo '[Hook] Review changes before push...' >&2
-
-code-insiders . 2>/dev/null
-
-echo '[Hook] Press Enter to continue with push or Ctrl+C to abort...' >&2
-read -r
