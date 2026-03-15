9e81f09627508af4b89fa029e80a62ac0d9ce01e
Yuta OHWATARI <y.ohwatari@cartahd.com>
2026-03-16 01:52:11 +0900

    refactor(zsh): remove redundant rust completion generation from rust.zsh

--- dot_zsh/plugins/rust.zsh
+++ dot_zsh/plugins/rust.zsh
@@ -2,15 +2,3 @@ export RUSTUP_HOME=$HOME/.rustup
 export CARGO_HOME=$HOME/.cargo
 
 path=($CARGO_HOME/bin $path)
-
-ZSH_COMPLETIONS="$HOME/.zsh/completions"
-
-if command -v rustup &>/dev/null; then
-  if [[ ! -f "$ZSH_COMPLETIONS/_rustup" ]]; then
-    rustup completions zsh > "$ZSH_COMPLETIONS/_rustup"
-  fi
-  if [[ ! -f "$ZSH_COMPLETIONS/_cargo" ]]; then
-    rustup completions zsh cargo > "$ZSH_COMPLETIONS/_cargo"
-  fi
-fi
-
